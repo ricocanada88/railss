03d659974c248df36810e535f39d8644e4e17af3
Theo <theo18112@gmail.com>
2022-12-08 23:55:50 +0700

    add validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    # /\A[^@\s]+@[^@\s]+\z/ : \A means ^(beginning of the line), \z means $(end of the line), \s means \f,\n,\r,\t,\v(white spaces, form feed)
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email" }
 end
